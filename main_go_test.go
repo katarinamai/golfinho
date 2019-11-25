b652735167ad67c055a225ba6253a0d82b466398
katarinamai <katarinamassakoinoue@gmail.com>
2019-11-25 20:47:51 -0300

    test

--- main_go_test.go
+++ main_go_test.go
@@ -2,6 +2,14 @@ package main
 
 import "testing"
 
-func TestMain_go(t *testing.T) {
+func TestMain_go_1(t *testing.T) {
 	budget(0)
+}
+
+func TestMain_go_2(t *testing.T) {
+	budget(10000000000000000000000000000000000000)
+}
+
+func TestMain_go_3(t *testing.T) {
+	budget(0.5)
 }
