0c9f44fa341d55e1c3ce85ea70422b2d288bc456
katarinamai <katarinamassakoinoue@gmail.com>
2019-11-25 21:42:51 -0300

    .

--- main_go_test.go
+++ main_go_test.go
@@ -4,12 +4,15 @@ import "testing"
 
 func TestMain_go_1(t *testing.T) {
 	budget(0, "S")
+	t.Log("S")
 }
 
 func TestMain_go_2(t *testing.T) {
 	budget(0, "N")
+	t.Log("N")
 }
 
 func TestMain_go_3(t *testing.T) {
 	budget(0.5, "")
+	t.Log("")
 }
