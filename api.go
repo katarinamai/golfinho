76fe73d3d070461715261733edda80020dedc03c
katarinamai <katarinamassakoinoue@gmail.com>
2019-12-11 22:47:19 -0300

    changes

--- api.go
+++ api.go
@@ -4,9 +4,7 @@ import (
 	"net/http"
 )
 
-type server struct{}
-
-func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func home2(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	switch r.Method {
 	case "GET":
@@ -29,8 +27,7 @@ func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 /*
 func main() {
-	s := &server{}
-	http.Handle("/", s)
+	http.HandleFunc("/", home)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 */
