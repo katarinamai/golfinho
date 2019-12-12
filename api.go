03a24e3036756f2b9c0bf7a28c1c2566f3f8f9d9
katarinamai <katarinamassakoinoue@gmail.com>
2019-12-11 22:33:55 -0300

    a new api

---
+++ api.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type server struct{}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	switch r.Method {
+	case "GET":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message": "get called"}`))
+	case "POST":
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(`{"message": "post called"}`))
+	case "PUT":
+		w.WriteHeader(http.StatusAccepted)
+		w.Write([]byte(`{"message": "put called"}`))
+	case "DELETE":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message": "delete called"}`))
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(`{"message": "not found"}`))
+	}
+}
+
+func main() {
+	s := &server{}
+	http.Handle("/", s)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
